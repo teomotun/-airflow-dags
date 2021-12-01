eb2f9589f7a2526d547853480d438aa545079501
Tolu Omotunde <omotundetolu@gmail.com>
2021-12-01 17:46:43 -0500

    changed s3 output bucket

--- sample_dag.py
+++ sample_dag.py
@@ -22,6 +22,6 @@ with DAG(dag_id='list_dag', schedule_interval='0 0 * * *', default_args=default_
 
     task_5 = BashOperator(task_id='task_5', bash_command='echo "pipeline done"')
 
-    task_6 = BashOperator(task_id='task_6', bash_command='aws s3 ls s3://demo-wcd/')
+    task_6 = BashOperator(task_id='task_6', bash_command='aws s3 ls s3://wcd-midterm-output/')
 
     tasks >> task_4 >> task_5 >> task_6
